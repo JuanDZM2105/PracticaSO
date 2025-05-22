6607b280121690a2767a7c8e8a125f751808d5fa
JuanDZM2105 <juandzm2105@gmail.com>
2025-05-22 06:28:15 -0500

    first commit

---
+++ GetProdsCon.py
@@ -0,0 +1,32 @@
+import threading
+from typing import Generic, TypeVar
+
+T = TypeVar('T')
+
+class GenProdCons(Generic[T]):
+    def __init__(self, size: int = 10):
+        if size <= 0:
+            raise ValueError("El tamaño del buffer debe ser mayor a cero.")
+
+        self.size = size
+        self.buffer = []
+        self.lock = threading.Lock()
+        self.empty_slots = threading.Semaphore(size)  # Cuántos espacios libres hay
+        self.full_slots = threading.Semaphore(0)      # Cuántos elementos disponibles hay
+
+    def put(self, item: T):
+        self.empty_slots.acquire()   # Espera espacio libre
+        with self.lock:
+            self.buffer.append(item)
+        self.full_slots.release()    # Señala elemento disponible
+
+    def get(self) -> T:
+        self.full_slots.acquire()    # Espera elemento disponible
+        with self.lock:
+            item = self.buffer.pop(0)
+        self.empty_slots.release()   # Señala espacio libre
+        return item
+    
+    def __len__(self):
+        with self.lock:
+            return len(self.buffer)
---
+++ RendezvousDEchange.py
@@ -0,0 +1,28 @@
+from typing import TypeVar, Generic
+import threading
+
+E = TypeVar('E')
+T = TypeVar('T')
+
+class RendezvousDEchange(Generic[E, T]):
+    def _init_(self):
+        self.condition = threading.Condition()
+        self.value: E | None = None
+        self.has_value = False
+
+    def echanger(self, value: E) -> T:
+        with self.condition:
+            if not self.has_value:
+                self.value = value
+                self.has_value = True
+                self.condition.wait()
+                result = self.value
+                self.has_value = False
+                self.condition.notify()
+                return result  # tipo T
+            else:
+                temp = self.value
+                self.value = value
+                self.condition.notify()
+                self.condition.wait()
+                return temp  # tipo T
